5566814236c6236357480df9f633988b328fafa1
haruto312 <haruto312@i.softbank.jp>
2026-08-27 22:39:25 +0900

    Add files via upload

---
+++ hito.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Bing → Hitomi.la 検索
+// @namespace    local
+// @version      1.0
+// @match        https://www.bing.com/search*
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const DOMAIN = 'hitomi.la';
+
+    const url = new URL(location.href);
+    const query = url.searchParams.get('q');
+
+    if (!query) return;
+
+    // 既に site: が付いている場合は変更しない
+    if (/\bsite:/i.test(query)) return;
+
+    url.searchParams.set('q', `${query} site:${DOMAIN}`);
+
+    location.replace(url.href);
+})();
